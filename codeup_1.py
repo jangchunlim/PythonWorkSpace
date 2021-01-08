86f2cbdd9edd31744878dbc763a0cd3a02c15539
charlie <charlie.jc.lim@gmail.com>
2021-01-08 16:27:13 +0900

    최소대금

---
+++ codeup_1.py
@@ -0,0 +1,20 @@
+def getLowestPrice(arrayPrice):
+    lowestPrice = None
+    for price in arrayPrice:
+        if lowestPrice is None:
+            lowestPrice = price
+        else:
+            if int(lowestPrice) > int(price):
+                lowestPrice = price
+    return lowestPrice
+
+pastaPrice = [input() for _ in range(3)]
+juicePrice = [input() for _ in range(2)]
+
+lowestPastPrice = getLowestPrice(pastaPrice)
+lowestJuicePrice = getLowestPrice(juicePrice)
+
+sumPrice = float(lowestPastPrice) + float(lowestJuicePrice)
+totalPrice = float(sumPrice) + float(sumPrice)/10
+
+print(totalPrice)
